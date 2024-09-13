506e6e294c13bf0a73e6313a47b0d286d1b7c05f
steef <stefanodegenkamp@gmail.com>
2024-09-13 10:16:47 +0200

    🪦 Remove current implementation of Naplify
    As we find working with the Symfony framework way to complex for this “simple” project.
    
    Instead, we would like to start with a more functional orientated approach, by just having a HTML
    and Javascript file.

--- assets/updateMinutes.js
+++
@@ -1,8 +0,0 @@
-function updateMinutes() {
-    const inputInMinutes = document.getElementById('inputInMinutes').value;
-    const displayInMinutes = document.getElementById('displayInMinutes');
-
-    displayInMinutes.innerText = `${inputInMinutes} minutes`;
-}
-
-window.updateMinutes = updateMinutes;
